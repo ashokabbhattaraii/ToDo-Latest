19ba34afdbfc31853c27932c2eeaeb0a74e8b0c1
Ashok <bhattaraiashok101@gmail.com>
2025-11-21 15:50:47 +0545

    Fixed

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const addList = document.querySelector('#addList')
 var inputField = document.querySelector('#todoField')
 const lists = document.querySelector("#lists")
 let listsData=[]
+let taskCompleted=false;
+const msg = document.querySelector('.msg')
 let num = 0;
 function saveToLocal() {
   localStorage.setItem('listOfTodo', JSON.stringify(listsData));
@@ -18,52 +20,47 @@ addList.addEventListener('click',()=>{
     num++;
     const todo = inputField.value.trim();
     if(!todo){
-        alert('Empty')
+        msg.innerHTML="Input Field should not be empty"
+        msg.style.color="#F44336"
     }else{
     const li = document.createElement('li');
-    const checkbox = document.createElement('input')
+    const checkbox = document.createElement('img')
+    checkbox.src="./materials/icons8-checkbox-50.png"
     const delImg = document.createElement('img')
     delImg.src="./materials/icons8-delete.svg"
     delImg.className="delImg"
-    checkbox.type="checkbox"
+    
     checkbox.className="markComplete"
     li.innerText=todo
     li.appendChild(checkbox)
     li.appendChild(delImg)
     
      listsData.push({
-      
+        sattus:taskCompleted,
         liElement:li,
         checkbox:checkbox,
         delIcon: delImg,
         text:todo
      });
-
+  
    saveToLocal(listsData)
    appendList(listsData,li)
+    msg.innerHTML="Task listed successsfully"
+        msg.style.color="green"
     inputField.value=""
     }
 
-
 })
 
-function saveToLocal(listsData){
-    localStorage.setItem('listOfTodo',JSON.stringify(listsData))
-}
 
 
-const logo = document.querySelector('#logo')
 
-logo.addEventListener('click',()=>{
-   const data = loadFromLocal()
-   console.log(data)
-})
 
 function appendList(listsData, li) {
   
   lists.appendChild(li);
-  
-   
+
+
     listsData.forEach((item, index) => {
       
         item.delIcon.addEventListener('click', () => {
@@ -71,12 +68,29 @@ function appendList(listsData, li) {
          
             listsData.splice(index, 1);
             item.liElement.remove();
+            msg.innerHTML="Task Deleted"
         });
+        item.checkbox.addEventListener('click',()=>{
+            toggleTodoStatus(item)
+            console.log(listsData)
+        })
     });
 }
 
+function toggleTodoStatus(todoObj) {
+    todoObj.sattus = !todoObj.sattus; 
+    todoObj.checkbox.src = todoObj.sattus
+        ? "./materials/icons8-checkbox-50 (1).png" 
+        : "./materials/icons8-checkbox-50.png"; 
 
+       saveToLocal();
 
+}
+
+window.addEventListener('load',()=>{
+    const tasks = loadFromLocal()
+    console.log(tasks)
+})
 
 
 
